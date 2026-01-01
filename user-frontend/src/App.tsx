b5d4b91d1f858d3c7dae74c987b2ac797a5991ad
Henrico de Souza <henrico.souza21@gmail.com>
2025-12-31 21:17:35 -0300

    feat: inicializa user-frontend com React, Vite, Tilwindcss e suas dependências

---
+++ user-frontend/src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-gray-800'>
+        <p className='text-white p-8'>Teste</p>
+      </div>
+    </>
+  )
+}
+
+export default App
