6a29f091e3f464d75bb263078e7cd16267692653
Henrico de Souza <henrico.souza21@gmail.com>
2026-01-01 22:22:47 -0300

    feat: cria UserList

---
+++ user-frontend/src/components/UserList.tsx
@@ -0,0 +1,70 @@
+import { useEffect, useState } from "react";
+import { API_BASE_URL } from "../services/api";
+
+interface User {
+    name: string;
+    email: string;
+    age: number;
+}
+
+export function UserList({ updateList }: { updateList: boolean }) {
+
+    const [users, setUsers] = useState<User[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            setLoading(true);
+            setError("");
+
+            try {
+                const response = await fetch(`${API_BASE_URL}/users`);
+
+                if (!response.ok) {
+                    throw new Error("Erro ao buscar usuários");
+                }
+
+                const data = await response.json();
+                setUsers(data);
+            } catch (error) {
+                setError("Erro ao carregar os usuários");
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchUsers();
+    }, [updateList]);
+
+    if (loading) {
+        return <div className="w-full max-w-md mx-auto mt-10 p-6 text-xl text-blue-500 flex justify-center items-center border rounded">Carregando usuários...</div>;
+    }
+
+    if (error) {
+        return <div className="w-full max-w-md mx-auto mt-10 p-6 text-xl text-red-500 flex justify-center items-center border rounded">{error}</div>;
+    }
+
+    if (users.length === 0) {
+        return <div className="w-full max-w-md mx-auto mt-10 p-6 text-xl text-gray-600 flex justify-center items-center border rounded">Nenhum usuário encontrado.</div>;
+    }
+
+    return (
+        <div className="flex min-h-screen items-center justify-center bg-gray-100 p-4">
+            <div className="w-full max-w-md rounded-lg bg-white p-8 shadow-xl">
+                <h2 className="mb-4 pb-5 border-b text-2xl font-semibold text-gray-800">
+                    Usuários
+                </h2>
+                <ul className="flex flex-col gap-4">
+                    {users.map((user, index) => (
+                        <li key={index} className="pb-2 flex flex-col gap-1 border-b hover:rounded-md hover:border hover:border-gray-400 hover:bg-gray-100 transition-all duration-100">
+                            <p className="pl-2"><strong>Nome:</strong> {user.name}</p>
+                            <p className="pl-2"><strong>Email:</strong> {user.email}</p>
+                            <p className="pl-2"><strong>Idade:</strong> {user.age}</p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    )
+}
