651cd7bf09fd524e4906fd7feb47b0d5136bbdc4
Henrico de Souza <henrico.souza21@gmail.com>
2026-01-01 22:27:33 -0300

    fix: ajusta App.tsx com react-router-dom

--- user-frontend/src/App.tsx
+++ user-frontend/src/App.tsx
@@ -1,13 +1,29 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { useState } from "react";
+import { UserForm } from "./components/UserForm"
+import { UserList } from "./components/UserList";
 
 
 function App() {
 
+  const [updateFlag, setUpdateFlag] = useState(false);
+
+  const handleUserCreated = () => setUpdateFlag(!updateFlag);
+
   return (
-    <>
-      <div className='bg-gray-800'>
-        <p className='text-white p-8'>Teste</p>
+    <Router>
+      <header className="p-3 bg-blue-900 flex gap-4 justify-end">
+        <Link to="/" className="px-4 py-2 text-white hover:bg-blue-950 rounded transition-colors duration-400">Novo Usuário</Link>
+        <Link to="/users" className="px-4 py-2 text-white hover:bg-blue-950 rounded transition-colors duration-400">Usuários</Link>
+      </header>
+
+      <div>
+        <Routes>
+          <Route path="/" element={<UserForm onUserCreated={handleUserCreated} />} />
+          <Route path="/users" element={<UserList updateList={updateFlag} />} />
+        </Routes>
       </div>
-    </>
+    </Router>
   )
 }
 
