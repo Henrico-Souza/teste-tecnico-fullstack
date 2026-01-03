99c3fa8baf10dedcc84070b9c015472a8424edb0
Henrico de Souza <henrico.souza21@gmail.com>
2026-01-02 21:18:11 -0300

    style: melhora UI no UserList

--- user-frontend/src/components/UserList.tsx
+++ user-frontend/src/components/UserList.tsx
@@ -38,15 +38,15 @@ export function UserList({ updateList }: { updateList: boolean }) {
     }, [updateList]);
 
     if (loading) {
-        return <div className="w-full max-w-md mx-auto mt-10 p-6 text-xl text-blue-500 flex justify-center items-center border rounded">Carregando usuários...</div>;
+        return <div className="w-full max-w-md mx-auto mt-10 p-6 text-xl text-blue-500 flex justify-center items-center border rounded bg-blue-50 drop-shadow-xl drop-shadow-blue-100">Carregando usuários...</div>;
     }
 
     if (error) {
-        return <div className="w-full max-w-md mx-auto mt-10 p-6 text-xl text-red-500 flex justify-center items-center border rounded">{error}</div>;
+        return <div className="w-full max-w-md mx-auto mt-10 p-6 text-xl text-red-500 flex justify-center items-center border rounded bg-red-50 drop-shadow-xl drop-shadow-red-100">{error}</div>;
     }
 
     if (users.length === 0) {
-        return <div className="w-full max-w-md mx-auto mt-10 p-6 text-xl text-gray-600 flex justify-center items-center border rounded">Nenhum usuário encontrado.</div>;
+        return <div className="w-full max-w-md mx-auto mt-10 p-6 text-xl text-gray-600 flex justify-center items-center border rounded bg-gray-50 drop-shadow-xl drop-shadow-gray-200">Nenhum usuário encontrado.</div>;
     }
 
     return (
@@ -55,9 +55,9 @@ export function UserList({ updateList }: { updateList: boolean }) {
                 <h2 className="mb-4 pb-5 border-b text-2xl font-semibold text-gray-800">
                     Usuários
                 </h2>
-                <ul className="flex flex-col gap-4">
+                <ul className="flex flex-col gap-4 border-b pb-4">
                     {users.map((user, index) => (
-                        <li key={index} className="pb-2 flex flex-col gap-1 border-b hover:rounded-md hover:border hover:border-gray-400 hover:bg-gray-100 transition-all duration-100">
+                        <li key={index} className="pb-2 flex flex-col gap-1 border border-gray-300 rounded-sm hover:bg-gray-200 bg-gray-100 transition-all duration-100">
                             <p className="pl-2"><strong>Nome:</strong> {user.name}</p>
                             <p className="pl-2"><strong>Email:</strong> {user.email}</p>
                             <p className="pl-2"><strong>Idade:</strong> {user.age}</p>
